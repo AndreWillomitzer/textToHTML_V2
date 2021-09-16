c556b91ea6fafb84da1fd7225745713117fba162
Andre Willomitzer <andre.willomitzer@gmail.com>
2021-09-16 19:48:51 -0400

    fixed -i error for nonexistent files, path.join().

--- textToHTML.js
+++ textToHTML.js
@@ -29,8 +29,7 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
   .alias('s', 'stylesheet')
   .command("--input", "Convert lines in a text file to HTML <p> tags.")
   .example("textToHTML: node textToHTML.js -i <filename>.").argv
-  //console.log(argv);
-//console.log(argv.file);
+
 const fs = require('fs');
 const path = require('path');
 let tempString;
@@ -48,89 +47,58 @@ if(argv.o === "./dist"){
         throw error;
       }
     });
-}/* else{
-  if(fs.existsSync(`./${argv.o}`)){
-    fs.rmdirSync(`./${argv.o}`,{recursive: true} , error=>{
-      if(error){
-        throw error;
-      }
-    });
-  }
-    fs.mkdir(`./${argv.o}`, error=>{
-      if(error){
-        throw error;
-      }
-    });
-}  */
-/* 
-if(argv.input.indexOf(":") != -1){
-    if(fs.existsSync(`./${argv.o}`)){
-      fs.rmdirSync(`./${argv.o}`,{recursive: true} , error=>{
-        if(error){
-          throw error;
-        }
-      });
-    }
-    fs.mkdir(`./${argv.o}`, error=>{
-      if(error){
-        throw error;
-      }
-    });
-  }else{
-    fs.mkdir(`${argv.o}`, error=>{
-      if(error){
-        throw error;
-      }
-    });
-  } 
-  */
-if(fs.lstatSync(argv.input).isDirectory()){ //if the input is a directory
-fs.readdirSync(argv.input).forEach(file =>{
-  let path = argv.input + "/"+ file;
-  console.log("Directory file names: ", file);
-  fs.readFile(path.toString(), 'utf-8', function(error, data){
-      //console.log("data in directory file: ", data);
-      const html = data
-      .split(/\r?\n\r?\n/)
-      .map(para =>
-        `\n<p>${para.replace(/\r?\n/, ' ')}</p>`
-      ).join(' ');
-
-  //console.log("html value:", html);
-  tempString = `<!DOCTYPE html>` + '\n'
-  + `<html>\n<head> \n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">` + `\n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
-  if(argv.s){
-    tempString = `<!DOCTYPE html>` + '\n'
-    + `<html>\n<head> \n<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">` + `\n<link rel="stylesheet" href="${argv.s}"> \n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
-  }
-    fs.writeFile(`${argv.output}/${file.split(".")[0]}.html`, tempString, error=>{
-      if(error){
-        throw error;
-      }
-    });
-  })
-})
-}else{ //if the input is a single file
-  fs.readFile(argv.input, 'utf8', function(error, data){
-      if(error){
-          throw error;
-      }
-      //console.log("Data value:", data);        
-      const html = data
-          .split(/\r?\n\r?\n/)
-          .map(para =>
-            `\n<p>${para.replace(/\r?\n/, ' ')}</p> </br>`
+}
+if(fs.existsSync(argv.input)){
+  if(fs.lstatSync(argv.input).isDirectory()){ //if the input is a directory
+    fs.readdirSync(argv.input).forEach(file =>{
+      //let path = argv.input + "/"+ file;
+      console.log("Directory file names: ", file);
+      fs.readFile(path.join(argv.input, file), 'utf-8', function(error, data){
+        //console.log("data in directory file: ", data);
+        const html = data
+        .split(/\r?\n\r?\n/)
+        .map(para =>
+          `\n<p>${para.replace(/\r?\n/, ' ')}</p>`
           ).join(' ');
-        tempString = `<!DOCTYPE html>` + '\n'
-        + `<html>\n<head> \n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">` + `\n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
-        if(argv.s){
+          
+          //console.log("html value:", html);
           tempString = `<!DOCTYPE html>` + '\n'
-          + `<html>\n<head> \n<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">` + `\n<link rel="stylesheet" href="${argv.s}"> \n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
-        }
-      fs.writeFile(`${argv.output}/${path.basename(argv.input, ".txt")}.html`, tempString, error=>{
+          + `<html>\n<head> \n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">` + `\n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
+          if(argv.s){
+            tempString = `<!DOCTYPE html>` + '\n'
+            + `<html>\n<head> \n<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">` + `\n<link rel="stylesheet" href="${argv.s}"> \n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
+          }
+          fs.writeFile(`${argv.output}/${path.basename(file, ".txt")}.html`, tempString, error=>{
+            if(error){
+              throw error;
+            }
+          });
+        })
+      })
+    }else{ //if the input is a single file
+      fs.readFile(argv.input, 'utf8', function(error, data){
         if(error){
           throw error;
         }
-      });
-  });
+        //console.log("Data value:", data);        
+        const html = data
+        .split(/\r?\n\r?\n/)
+        .map(para =>
+          `\n<p>${para.replace(/\r?\n/, ' ')}</p> </br>`
+          ).join(' ');
+          tempString = `<!DOCTYPE html>` + '\n'
+          + `<html>\n<head> \n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">` + `\n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
+          if(argv.s){
+            tempString = `<!DOCTYPE html>` + '\n'
+            + `<html>\n<head> \n<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">` + `\n<link rel="stylesheet" href="${argv.s}"> \n</head>\n<body>` + `${html}` + `\n</body>\n</html>`;
+          }
+          fs.writeFile(`${argv.output}/${path.basename(argv.input, ".txt")}.html`, tempString, error=>{
+            if(error){
+              throw error;
+            }
+          });
+        });
+      }      
+}else{
+  console.log("File or folder does not exist.");
 }
